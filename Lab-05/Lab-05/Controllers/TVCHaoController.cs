f1532d57ad8b6ad9c0f8327de5ed0a6a5e4eb868
haoduong24 <164982752+haoduong24@users.noreply.github.com>
2024-05-27 08:55:35 +0700

    Lab_05

---
+++ Lab-05/Lab-05/Controllers/TVCHaoController.cs
@@ -0,0 +1,41 @@
+﻿using Lab_05.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_05.Controllers
+{
+    public class TVCHaoController : Controller
+    {
+        private static List<Models.TVCSong> tVCHaos = new List<Models.TVCSong>()
+        {
+            new Models.TVCSong{id=100,tvctitle="Duong Hao",tvcAuthor="k22cnt1",tvcArtist="Haod",tvcYearRelease=2023},
+            new Models.TVCSong{id=100,tvctitle="Duong Haoo",tvcAuthor="k22cnt",tvcArtist="Haoduong",tvcYearRelease=2024}
+        };
+        // GET: TVCHao
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //get create song
+        public ActionResult TVCCreate()
+        {
+            var tvcSong = new TVCSong();
+            return View();
+        }
+        // post create song
+        [HttpPost]
+        public ActionResult TVCCreate(TVCSong tvcSong)
+        {
+            if(!ModelState.IsValid) // neu co loi xay ra
+            { 
+                return View(tvcSong); 
+            }
+            // neu du lieu dung -> luu vao danh sach bai hat
+            tVCHaos.Add(tvcSong);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Lab-05/Lab-05/Models/TVCSong.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Lab_05.Models
+{
+    public class TVCSong
+    {
+        public int id { get; set; }
+        [DisplayName("Tieu de")]
+        [Required(ErrorMessage ="TVC: Hay nhap tieu de")]
+        public string tvctitle { get; set; }
+        [DisplayName("Tac gia")]
+        [Required(ErrorMessage = "TVC: Hay nhap tac gia")]
+        public string tvcAuthor { get; set; }
+        [DisplayName("Nghe si")]
+        [Required(ErrorMessage = "TVC: Hay nhap nghe si")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "TVC: do dai trong khoang [2-50]")]
+        public string tvcArtist { get; set; }
+        [DisplayName("Nam xuat ban")]
+        [Required(ErrorMessage = "TVC: Hay nhap nam xuat ban")]
+        [RegularExpression(@"[0-9]{4}",ErrorMessage ="TVC: Nam xuat ban pai la so co 4 ky tu")]
+        [Range(1900,2020,ErrorMessage ="TVC Nam xuat ban trong khoang 1900 - 2020")]
+        public int tvcYearRelease { get; set; }
+    }
+}
